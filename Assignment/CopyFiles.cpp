87c0f15bf98d88ea7b2df224a928e445f922b3ed
Rudals <2153rud@gmail.com>
2022-10-13 00:24:51 +0900

    ANSI 표준 파일 입출력 함수를 이용한 파일 복사 프로그램
    ANSI 표준 파일 입출력 함수를 이용한 파일 복사 프로그램

---
+++ Assignment/CopyFiles.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	/*std::ifstream InputFile;
+	InputFile.open("../PracticeMaterial/OriginFile.txt", std::ios::in);*/
+	/*std::ofstream OutputFile;
+	OutputFile.open("../PracticeMaterial/CopyFile.txt", std::ios::out | std::ios::trunc);*/
+	std::ifstream InputFile("../PracticeMaterial/OriginFile.txt", std::ios::in);
+	std::ofstream OutputFile("../PracticeMaterial/CopyFile.txt", std::ios::out | std::ios::trunc);
+	char buffer[256] = { 0, };
+	
+	if (!InputFile.fail())
+	{
+		while (InputFile.getline(buffer, 256))
+		{
+			OutputFile << buffer << std::endl;
+		}
+	}
+
+	InputFile.close();
+	OutputFile.close();
+
+	return 0;
+}
