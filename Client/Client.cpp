aa02f41af924b14f79c2e3ad2c06177c3a206ac5
Rudals <2153rud@gmail.com>
2022-10-12 17:58:53 +0900

    Server Socket, Client Socket 생성 및 입출력
    Server Socket, Client Socket 생성 및 입출력

---
+++ Client/Client.cpp
@@ -0,0 +1,68 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+#include <cstdlib>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using std::cout;
+using std::endl;
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char buffer[30];
+	int strLen;
+
+	if (argc != 3)
+	{
+		cout << "Usage : " << argv[0] << "<port>" << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+	{
+		ErrorHandling("socket() error!");
+	}
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+	{
+		ErrorHandling("connect() error!");
+	}
+
+	strLen = recv(hSocket, buffer, sizeof(buffer) - 1, 0);
+	if (strLen == -1)
+	{
+		ErrorHandling("read() error!");
+	}
+
+	cout << "Message from server : " << buffer << endl;
+
+	closesocket(hSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+	cout << message << endl;
+	exit(1);
+}
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,12 +1,75 @@
 #include <WinSock2.h>
+#include <iostream>
+#include <cstdlib>
 
 #pragma comment(lib, "ws2_32.lib")
+
+using std::cout;
+using std::endl;
+
+void ErrorHandling(const char* message);
 
 int main(int argc, char* argv[])
 {
 	WSADATA wsaData;
-	
-	
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+	int szClntAddr;
+	char buffer[] = "Hello World! and Socket!";
+
+	if (argc != 2)
+	{
+		cout << "Usage : " << argv[0] << "<port>" << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServSock == INVALID_SOCKET)
+	{
+		ErrorHandling("socket() error!");
+	}
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+	{
+		ErrorHandling("bind() error!");
+	}
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+	{
+		ErrorHandling("listen() error!");
+	}
+
+	szClntAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+
+	if (hClntSock == INVALID_SOCKET)
+	{
+		ErrorHandling("accept() error!");
+	}
+
+	send(hClntSock, buffer, sizeof(buffer), 0);
+
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	WSACleanup();
 
 	return 0;
 }
+
+void ErrorHandling(const char* message)
+{
+	cout << message << endl;
+	exit(1);
+}
+
