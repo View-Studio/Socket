ca32905e204d3fcee4475554e12fcc05717bf504
Rudals <2153rud@gmail.com>
2022-10-17 01:13:31 +0900

    WinSock Library 초기화 코드 추가
    WinSock Library 초기화 코드 추가

--- Assignment/Endian_Conv.cpp
+++ Assignment/Endian_Conv.cpp
@@ -6,12 +6,20 @@
 using std::cout;
 using std::endl;
 
+void ErrorHandling(const char* message);
+
 int main(int argc, char* argv[])
 {
+	WSADATA wsaData;
 	unsigned short HostPort = 0x1234;
 	unsigned short NetPort = 0;
 	unsigned long HostAddr = 0x12345678;
 	unsigned long NetAddr = 0;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
 
 	NetPort = htons(HostPort);
 	NetAddr = htonl(HostAddr);
@@ -27,5 +35,13 @@ int main(int argc, char* argv[])
 	cout << "Host ordered port : " << std::hex << NetPort << endl;
 	cout << "Host ordered address : " << std::hex << NetAddr << endl;
 
+	WSACleanup();
+
 	return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+	cout << message << endl;
+	exit(1);
 }
