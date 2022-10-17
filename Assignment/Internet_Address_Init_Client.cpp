55bf982ad678e7678cdf3dd02936d46868cc4451
Rudals <2153rud@gmail.com>
2022-10-17 13:05:57 +0900

    Server와 Client에서 Internet Address 정보를 초기화 하는 프로그램
    Server와 Client에서 Internet Address 정보를 초기화 하는 프로그램

---
+++ Assignment/Internet_Address_Init_Client.cpp
@@ -0,0 +1,32 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		cout << "WSAStartup Error" << endl;
+	}
+
+	SOCKADDR_IN Addr;
+	const char* IP = "211.231.168.13";
+	const char* PORT = "5039";
+
+	memset(&Addr, 0, sizeof(Addr));
+	Addr.sin_family = AF_INET;
+	Addr.sin_addr.s_addr = inet_addr(IP);
+	Addr.sin_port = htons(atoi(PORT));
+
+	WSACleanup();
+
+	return 0;
+}
