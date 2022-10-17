abc92e8083b4645cdc106d89f9df91b998e64f09
Rudals <2153rud@gmail.com>
2022-10-17 13:07:14 +0900

    WinSockAPI 전용 함수로 Server와 Client에서 Internet Address 정보를 초기화 하는 프로그램
    WinSockAPI 전용 함수로 Server와 Client에서 Internet Address 정보를 초기화 하는 프로그램

---
+++ Assignment/String_Address_Trans_WSA.cpp
@@ -0,0 +1,36 @@
+#undef UNICODE
+#undef _UNICODE
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	const char* StrAddr = "212.155.168.102:9403";
+
+	char StrBuffer[50];
+	int Size = 0;
+	SOCKADDR_IN ServAddr;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "WSAStartup Error" << endl;
+	}
+
+	Size = sizeof(ServAddr);
+	WSAStringToAddress(StrAddr, AF_INET, NULL, (SOCKADDR*)&ServAddr, &Size);
+
+	Size = sizeof(StrBuffer);
+	WSAAddressToString((SOCKADDR*)&ServAddr, sizeof(ServAddr), NULL, StrBuffer, &Size);
+
+	cout << "Second Conv Result : " << StrBuffer << endl;
+
+	WSACleanup();
+
+	return 0;
+}
